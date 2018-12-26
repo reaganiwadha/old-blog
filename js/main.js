f0b4d9e1ba6ab416a987eddcbba4fa461721042e
reaganiwadha <reaganiwadha@gmail.com>
2018-12-26 21:40:00 +0700

    i felt wrong

--- js/main.js
+++ js/main.js
@@ -1,18 +1,25 @@
-var typeasfast = ">Type the query as fast as possible"
-var i = 0;
-var blink = false;
+//DOM VARIABLES
+$queryAsFast = $('#queryasfast');
+$startMenu = $('#startmenu');
 
-var interval = setInterval(function(){
-    $('#queryasfast').text($('#queryasfast').text() + typeasfast[i]);
-    i++;
-    if(i == typeasfast.length){
-        setInterval(function(){
-            blink 
-            ? $('#queryasfast').text($('#queryasfast').text()+'_')
-            : $('#queryasfast').text(typeasfast);
-            blink = !blink;
-        },600);
-        clearInterval(interval);
+$typeasfast = "Type the query as fast as possible"
+
+var textTypeAnim = setInterval(function(){
+    $queryAsFast.text($queryAsFast.text() + $typeasfast[$queryAsFast.text().length]);
+    if($queryAsFast.text().length == $typeasfast.length){
+        clearInterval(textTypeAnim);
+        $startMenu.addClass('hideme');
     }
 },50)
 
+
+
+var s = alasql('CREATE TABLE one (two INT)');
+var ins = alasql('INSERT INTO one (two) VALUES (20)')
+var ss = alasql('SELECT two FROM one')
+
+console.log(ss[0]["two"]);
+
+function executeQuery(){
+    $('#excuseme').text(alasql($('#querybox').val()));
+}
