ca6608608e5bc77c5b0c3d14cda10963b7dd0def
reaganiwadha <reaganiwadha@gmail.com>
2018-12-27 09:23:24 +0700

    its centered

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 //DOM VARIABLES
 $queryAsFast = $('#queryasfast');
 $startMenu = $('#startmenu');
+$startButton = $('#startbutton');
 
 $typeasfast = "Type the query as fast as possible"
 
@@ -8,11 +9,13 @@ var textTypeAnim = setInterval(function(){
     $queryAsFast.text($queryAsFast.text() + $typeasfast[$queryAsFast.text().length]);
     if($queryAsFast.text().length == $typeasfast.length){
         clearInterval(textTypeAnim);
-        $startMenu.addClass('hideme');
     }
 },50)
 
-
+//when started
+$startButton.click(function(){
+    $startMenu.addClass('hideme');
+})
 
 var s = alasql('CREATE TABLE one (two INT)');
 var ins = alasql('INSERT INTO one (two) VALUES (20)')
