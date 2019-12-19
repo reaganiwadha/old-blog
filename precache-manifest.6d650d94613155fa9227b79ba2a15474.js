33b45398fbc7adbaaafb2d8fec2e1b6f5368a7bd
Reagan Iwadha <reaganiwadha@protonmail.com>
2019-12-19 09:11:22 +0700

    Updates

---
+++ precache-manifest.6d650d94613155fa9227b79ba2a15474.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fad681b9d6b6bd0db9ea4862653c9db9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b586de6a1aa63bc2d1f",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5d990762d7f02085715d",
+    "url": "/static/js/2.a89c724f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.a89c724f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4b586de6a1aa63bc2d1f",
+    "url": "/static/js/main.8cb5a8e5.chunk.js"
+  },
+  {
+    "revision": "c190029e7064f9d168d0",
+    "url": "/static/js/runtime-main.1c86c0cb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
